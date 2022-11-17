81d8dade753480bd89852773a6654cb4f25312b4
Kevin Conboy <kev@alternate.org>
2022-11-16 18:51:55 -0700

    serverless?

---
+++ api/index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import jsonGraphqlExpress from "json-graphql-server";
+import data from "./tweets-gql";
+
+const PORT = 3000;
+const app = express();
+
+app.use("/graphql", jsonGraphqlExpress(data));
+app.listen(PORT);
